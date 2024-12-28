be878f0c3a91bac31253c03ee0af52ab6837248f
iRpro16 <rumeoisidoro16@gmail.com>
2024-12-28 17:07:31 -0500

    ship factory function

---
+++ src/components/ship.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @param {Number} length - the length of the ship
+ */
+export function Ship(length) {
+    // number of hits
+    let hits = 0;
+    // is sunk
+    let sunk = false;
+
+    // hit() method
+    const hit = () => {
+        if (!sunk) hits++;
+        isSunk(hits);
+        return hits;
+    }
+
+    // isSunk() method
+    const isSunk = (numHits) => {
+        if (numHits === length) sunk = true;
+        return sunk;
+    }
+
+    // get num hits
+    const getHits = () => hits;
+
+    return {
+        length: length,
+        getHits,
+        hit, 
+        isSunk
+    }
+}
+
+module.exports = Ship;
